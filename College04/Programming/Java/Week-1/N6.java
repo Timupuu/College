ebea86236d489c348388a7a7456bf33bc2956c04
Arjan Timalsina <timupuu@kali.kali.aut>
2025-11-10 18:15:14 +0545

    Initial Commit

---
+++ College04/Programming/Java/Week-1/N6.java
@@ -0,0 +1,15 @@
+
+/**
+ * Write a description of class N6 here.
+ * This is a new file for the first day of workshop class.
+ * @author (Arjan)
+ * @version (11/06/2025)
+ */
+public class N6
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello World");
+        System.out.println("My name is \nArjan Timalsina");
+    }
+}
