07593c5248d55d5240b87a5fd27c8d49c2eebe23
Arjan Timalsina <timupuu@kali.kali.aut>
2025-11-21 14:11:43 +0545

    date nov 21

---
+++ College04/sem1/Programming/Java/Week-3/j1120.java
@@ -0,0 +1,17 @@
+
+/**
+ * Write a description of class j1120 here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class j1120
+{
+ public static void main(String[] args){
+     int a = 10;
+     int b = 20;
+     //boolean c = (a>b)? true : false;
+     String c = (a>b)? "a is greater than b":"b is greater than a";
+     System.out.println(c);
+ }
+}
---
+++ College04/sem1/Programming/Java/Week-3/j11202.java
@@ -0,0 +1,77 @@
+import java.util.Scanner;
+
+/**
+ * Write a description of class j11202 here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class j11202
+{
+    public static final double PI = 3.14;
+    public static void main(String[] args){
+        
+        /**
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the value for a:");
+        int a = scanner.nextInt();
+        System.out.println("Enter the value for b:");
+        int b = scanner.nextInt();
+        System.out.println("Enter the operator:");
+         char opp = scanner.next().charAt(0);
+        if(opp == '+'){
+            System.out.println("The sum is "+ (a+b));
+        }
+        else if(opp == '-'){
+            System.out.println("The difference is "+ (a-b));
+        }
+        else if(opp == '*'){
+            System.out.println("The product is "+ (a*b));
+        }
+        else if(opp == '/'){
+            System.out.println("The quitent is "+ (a+b));
+        }
+        */
+       
+       //Perimeter
+       /** Scanner scanner = new Scanner(System.in);
+       System.out.println("Enter the value for length:");
+       int x = scanner.nextInt();
+       System.out.println("Enter the value for breath:");
+       int y = scanner.nextInt();
+       int peri = 2*(x+y);
+       System.out.println("The perimeter is "+ peri);
+       */
+      
+      
+      //Average
+      /**Scanner scanner = new Scanner(System.in);
+      int[] a = new int[3];
+      for(int i = 0; i<3;i++){
+          System.out.println("Enter the value of number " + (i+1));
+          a[i]=scanner.nextInt();
+        }
+        int total = a[0]+a[1]+a[2];
+        int avg = total/3;
+        System.out.println("The average value is " + avg);
+        */
+       // Average of 4 walls
+       /**Scanner scanner = new Scanner(System.in);
+       System.out.println("Enter the length of walls");
+       int a = scanner.nextInt();
+       System.out.println("Enter the breath of breath");
+       int b = scanner.nextInt();
+       System.out.println("Enter the height of walls");
+       int h = scanner.nextInt();
+       int area = 2*h*(a+b);
+       System.out.println("The area of the walls is"+ area);
+       */
+      
+      /**Scanner scanner = new Scanner(System.in);
+      System.out.println("Enter the radius of the circle");
+      double r = scanner.nextDouble();
+      double area = PI*r*r;
+      System.out.println("The value of the area is 2" + area);
+      */
+    }
+}
