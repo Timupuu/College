f7efb848ede456f4b32e8983f0d73b91a4317441
Arjan Timalsina <timupuu@kali.kali.aut>
2025-12-05 14:10:31 +0545

    date dec 5 and previous days

---
+++ College04/sem1/Programming/Java/j1202/jj1202.java
@@ -0,0 +1,32 @@
+
+/**
+ * Write a description of class jj1202 here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class jj1202
+{
+    public static void main(String [] args){
+    //for loop 
+    for(int i=0;i<4;i++){
+    for(int j = 0;j<4;j++){
+    System.out.print("*");
+    }
+    System.out.println();
+    }
+    //while loop
+    /**int i = 3;
+    while(i<8){
+    System.out.println("Hello World");
+    i++;
+    }
+    System.out.println(" ");
+    int j = 5;
+    while(j>0){
+    System.out.println("Hello world");
+    j--;
+    }*/
+    }
+    
+}
